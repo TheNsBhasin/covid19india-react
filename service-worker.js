4af5e8929923f1b6aa7d918166de1bec4e858e59
TheNsBhasin <NsBhasinCool@gmail.com>
2020-05-07 20:00:14 +0000

    Deploying to gh-pages from  @ e963433f12c6f39af573db3a382400c746a7046d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6841bde01ae9192b0d50fa3d66ed4959.js"
+  "/precache-manifest.a0325ee19daff44b6de6a7f7257295b7.js"
 );
 
 self.addEventListener('message', (event) => {
